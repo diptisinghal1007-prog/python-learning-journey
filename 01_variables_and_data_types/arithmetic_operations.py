1646dc9e0dc07fd98d149ce76778f935f452de7b
diptisinghal1007-prog <diptisinghal1007@gmail.com>
2026-07-05 15:47:32 +0530

    Add arithmetic operations in arithmetic_operations.py
    Implemented basic arithmetic operations including sum, difference, product, division, integer division, modulo, and exponentiation.

---
+++ 01_variables_and_data_types/arithmetic_operations.py
@@ -0,0 +1,18 @@
+#-----------Arithmetic Operations-----------
+
+num1 = float(input("Enter the first number : "))
+num2 = float(input("Enter the second number : "))
+sum_result = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+division = num1 / num2
+int_division = num1 // num2
+modulo = num1 % num2
+exponentiation = num1 ** num2
+print( "Sum :", sum_result)
+print("Difference :", difference)
+print("Product :", product)
+print("Division :", division)
+print("Integer Division :", int_division)
+print("Modulo :", modulo)
+print("Exponentiation :", exponentiation)
