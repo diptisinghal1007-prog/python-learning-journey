a45f1ae6e9f6c6b408b716ee49f22254293a4b41
diptisinghal1007-prog <diptisinghal1007@gmail.com>
2026-07-05 15:43:10 +0530

    Add Student Information program

---
+++ 01_variables_and_data_types/student_information.py
@@ -0,0 +1,8 @@
+#-----------Student Information----------------
+
+Name = input("Enter your name : ")
+Age = input("Your age : ")
+City = input("The city you live in :")
+print(Name)
+print(Age)
+print(City)
